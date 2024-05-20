73115748c044eddafbf2aaca478eefd5b97e0adf
Hundaol16 <111746724+Hundaol16@users.noreply.github.com>
2024-05-20 10:12:19 +0200

    Int

--- Website.js
+++ Website.js
@@ -13,3 +13,20 @@ function showSlides() {
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    searchResults = document.getElementById('searchResults');
+    searchResults.innerHTML = ''; // Clear previous results
+    // Simulated list of items to search through
+    var items = ['UCW Ready to Play "I Can Be Your Hero, Baby" e-sports build. '];
+    for (i = 0; i < items.length; i++) {
+        if (items[i].toUpperCase().indexOf(filter) > -1) {
+            // If the search term matches an item, display it
+            var result = document.createElement('div');
+            result.textContent = items[i];
+            searchResults.appendChild(result);
+        }
+    }
+}
